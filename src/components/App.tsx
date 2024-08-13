537543b189c7991f16b519ab1a47057ed7dc4e5f
sasha1985y <kuyancev1985@mail.ru>
2024-08-13 04:17:22 +0300

    исправил все кроме отправки на сервер

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -27,7 +27,7 @@ function App() {
   const editTodoHandler = (todo: Todo) => {
     const updatePatchTodo = async () => {
       const { id, ...updateData } = todo;
-      await axios.patch(`https://cd80175.tw1.ru/todos/${id}/`, updateData);
+      await axios.patch(`https://cd80175.tw1.ru/todos/${id}/`, updateData)
       // Ваши обновления...
       const updatedTodos = todos.map((todo) => {
         if(todo.id === id) {
@@ -65,77 +65,82 @@ function App() {
   }, [])
 
   return (
-    <div className="min-vh-100 bg-secondary position-relative">
-      <div className="h-5 d-inline-block"></div>
-      <h1 className="text-center">Todo App</h1>
-      <div className="d-flex justify-content-center hstack gap-2">
-        <input 
-          type="text"
-          id="myInput" 
-          maxLength={200}
-          className={`w-75 bg-body-secondary ${name.trim() === "" ? "rounded-pill" : "rounded-start-pill"}`}
-          placeholder="Add todo here..."
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <div onClick={addTodoHandler} className={`w-20 bg-body-secondary rounded-end-pill border border-dark-subtle border-3 ${name.trim() === "" ? "invisible" : ""}`}>
-          <i className="text-success fa-solid fa-square-plus"></i>
-        </div>
-      </div>
-      <div className="h-5 d-inline-block"></div>
-      <div className="row">
-        <div className="col-md-1"></div>
-        <ul className="col d-inline-block list-group">
-          {todos?.map((todo: Todo) => {
-            const chunkedName = todo.name.match(/.{1,49}/g);
-            return (
-              <li key={todo.id} className="col bg-secondary-subtle mb-1 rounded-4 list-group-item">
-                <p className="text-center" onClick={() => {
-                  setEditStatus(todo.status)
-                  setEditName(todo.name)
-                  setEditTodo(todo)
-                  setOpenEditUI(true)
-                }}>
-                  <div key={todo.id} className="todo-item">
-                    {chunkedName && chunkedName.map((chunk, chunkIndex) => (
-                      <span key={chunkIndex}>
-                        {chunk}
-                        {chunkIndex < chunkedName.length - 1 && <i>&#8211;<br></br></i>}
-                      </span>
-                    ))}
-                  </div>
-                </p>
-                {" "}
-                <div className="row">
-                  <span className="col-md-7 text-end text-light-emphasis">{todo.status ? "выполнена" : "не выполнена"}</span>
-                  <i onClick={() => deleteTodoHandler(todo.id)} className="col-md-5 text-end text-danger fs-3 bottom-0 end-0 fa-regular fa-trash-can"></i>
-                </div>
-            </li>
-            );
-          })}
+    <>
+      <div className={`row min-vh-100 h-15 w-20 bg-secondary ${openEditUI ? "" : "d-none"}`}>
+        <div className="h-5 d-inline-block"></div>
+        <div className="col-md-2"></div>
+        <ul className="col bg-body-secondary rounded-4">
+          <li className="d-flex justify-content-around hstack gap-2">
+            <span className="fs-6">Редактор задачи</span>
+            < input type="checkbox" checked={!editStatus} onChange={() => setEditStatus(!editStatus)}/>
+            <i>статус</i>
+            <i onClick={() => setOpenEditUI(false)} className="text-danger fs-5 fa-solid fa-xmark"></i>
+          </li>
+          <li className="input-group">
+            <textarea className="form-control" maxLength={200} value={editName} onChange={(e) => setEditName(e.target.value)}></textarea>
+          </li>
+          <li>
+            <button onClick={() => editTodo && editTodoHandler(editTodo)} className="btn btn-success">обновить</button>
+          </li>
         </ul>
-        <div className="col-md-4">
-          <div className={`position-fixed row h-15 w-20 bg-secondary ${openEditUI ? "" : "invisible"}`}>
-            <div className="col-md-2"></div>
-            <ul className="col bg-body-secondary rounded-4">
-              <li className="d-flex justify-content-around hstack gap-2">
-                <span className="fs-6">Редактор</span>
-                < input type="checkbox" checked={editStatus} onChange={() => setEditStatus(!editStatus)}/>
-                <i>статус</i>
-                <i onClick={() => setOpenEditUI(false)} className="text-danger fs-5 fa-solid fa-xmark"></i>
-              </li>
-              <li className="input-group">
-                <textarea className="form-control" maxLength={200} value={editName} onChange={(e) => setEditName(e.target.value)}></textarea>
-              </li>
-              <li>
-              <button onClick={() => editTodo && editTodoHandler(editTodo)} className="btn btn-success">обновить</button>
-              </li>
-            </ul>
-            <div className="col-md-2"></div>
+        <div className="col-md-2"></div>
+        <div className="h-5 d-inline-block"></div>
+      </div>
+      <div className={`min-vh-100 bg-secondary position-relative ${!openEditUI ? "" : "d-none"}`}>
+        <div className="h-5 d-inline-block"></div>
+        <h1 className="text-center">Todo App</h1>
+        <div className="row input-group input-group-sm mb-3">
+          <div className="col-md-3"></div>
+          <input 
+            type="text"
+            id="myInput" 
+            maxLength={200}
+            className={`form-control col bg-body-secondary ${name.trim() === "" ? "rounded-pill" : "rounded-start-pill"}`}
+            placeholder="Add todo here..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <div onClick={addTodoHandler} className={`col-md-1 w-20 bg-body-secondary rounded-end-pill border border-dark-subtle border-3 ${name.trim() === "" ? "invisible" : ""}`}>
+            <i className="text-success fa-solid fa-square-plus"></i>
           </div>
+          <div className={name.trim() === "" ? "col-md-2" : "col-md-3"}></div>
+        </div>
+        <div className="h-5 d-inline-block"></div>
+        <div className="row">
+          <div className="col-md-3"></div>
+          <ul className="col d-inline-block list-group">
+            {todos?.map((todo: Todo) => {
+              const chunkedName = todo.name.match(/.{1,49}/g);
+              return (
+                <li key={todo.id} className="col bg-secondary-subtle mb-1 rounded-4 list-group-item">
+                  <p className="text-center" onClick={() => {
+                    setEditStatus(todo.status)
+                    setEditName(todo.name)
+                    setEditTodo(todo)
+                    setOpenEditUI(true)
+                  }}>
+                    <div key={todo.id} className="todo-item">
+                      {chunkedName && chunkedName.map((chunk, chunkIndex) => (
+                        <span key={chunkIndex}>
+                          {chunk}
+                          {chunkIndex < chunkedName.length - 1 && <i>&#8211;<br></br></i>}
+                        </span>
+                      ))}
+                    </div>
+                  </p>
+                  {" "}
+                  <div className="row">
+                    <span className="col-md-7 text-end text-light-emphasis">{!todo.status ? "выполнена" : "не выполнена"}</span>
+                    <i onClick={() => deleteTodoHandler(todo.id)} className="col-md-5 text-end text-danger fs-3 bottom-0 end-0 fa-regular fa-trash-can"></i>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="col-md-3"></div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
